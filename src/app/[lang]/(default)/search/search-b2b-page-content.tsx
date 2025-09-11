b8837c749b1ba1c155ee2cb18afbdac00e8972cd
loziogigio <yiresse.abia@gmail.com>
2025-09-11 19:34:19 +0200

    multi tab search

--- src/app/[lang]/(default)/search/search-b2b-page-content.tsx
+++ src/app/[lang]/(default)/search/search-b2b-page-content.tsx
@@ -5,14 +5,17 @@ import { SearchFiltersB2B } from '@components/search/filters-b2b';
 import SearchTopBar from '@components/search/search-top-bar';
 import Container from '@components/ui/container';
 import { Element } from 'react-scroll';
+import SearchTabs from '@components/search/search-tabs';
 
 export default function SearchB2BPageContent({ lang }: { lang: string }) {
   return (
     <Container>
+      
       {/* Static horizontal filter */}
       <CategoryScrollFilter lang={lang}/>
-
+      <SearchTabs lang={lang} />
       <Element name="grid" className="flex pb-16 pt-7 lg:pt-7 lg:pb-20">
+       
         <div className="sticky hidden h-full lg:pt-4 shrink-0 ltr:pr-8 rtl:pl-8 xl:ltr:pr-16 xl:rtl:pl-16 lg:block w-80 xl:w-96 top-16">
           <SearchFiltersB2B lang={lang} />
         </div>
